41f61c48488f39e21e3fc2ccb6734b467ba82485
Charles Feval <c@feval.ca>
2021-03-30 20:32:31 -0700

    Move to Typescript, onion architecture

--- src/infrastructure/botActivityHandler.ts
+++ src/infrastructure/botActivityHandler.ts
@@ -1,15 +1,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const {
-    TurnContext,
+import { TurnContext } from "botbuilder-core";
+
+import {
     MessageFactory,
     TeamsActivityHandler,
     CardFactory,
     ActionTypes
-} = require('botbuilder');
+} from 'botbuilder';
 
-class BotActivityHandler extends TeamsActivityHandler {
+export class BotActivityHandler extends TeamsActivityHandler {
     constructor() {
         super();
         /*  Teams bots are Microsoft Bot Framework bots.
@@ -22,27 +23,27 @@ class BotActivityHandler extends TeamsActivityHandler {
                     Learn more: https://aka.ms/teams-register-bot. 
         */
         // Registers an activity event handler for the message event, emitted for every incoming message activity.
-        this.onMessage(async (context, next) => {
+        this.onMessage(async (context: TurnContext, next: any) => {
             TurnContext.removeRecipientMention(context.activity);
             switch (context.activity.text.trim()) {
-            case 'Hello':
-                await this.mentionActivityAsync(context);
-                break;
-            default:
-                // By default for unknown activity sent by user show
-                // a card with the available actions.
-                const value = { count: 0 };
-                const card = CardFactory.heroCard(
-                    'Lets talk...',
-                    null,
-                    [{
-                        type: ActionTypes.MessageBack,
-                        title: 'Say Hello',
-                        value: value,
-                        text: 'Hello'
-                    }]);
-                await context.sendActivity({ attachments: [card] });
-                break;
+                case 'Hello':
+                    await this.mentionActivityAsync(context);
+                    break;
+                default:
+                    // By default for unknown activity sent by user show
+                    // a card with the available actions.
+                    const value = { count: 0 };
+                    const card = CardFactory.heroCard(
+                        'Lets talk...',
+                        undefined,
+                        [{
+                            type: ActionTypes.MessageBack,
+                            title: 'Say Hello',
+                            value: value,
+                            text: 'Hello'
+                        }]);
+                    await context.sendActivity({ attachments: [card] });
+                    break;
             }
             await next();
         });
@@ -51,21 +52,18 @@ class BotActivityHandler extends TeamsActivityHandler {
     /**
      * Say hello and @ mention the current user.
      */
-    async mentionActivityAsync(context) {
+    async mentionActivityAsync(context: TurnContext) {
         const TextEncoder = require('html-entities').XmlEntities;
 
         const mention = {
             mentioned: context.activity.from,
-            text: `<at>${ new TextEncoder().encode(context.activity.from.name) }</at>`,
+            text: `<at>${new TextEncoder().encode(context.activity.from.name)}</at>`,
             type: 'mention'
         };
 
-        const replyActivity = MessageFactory.text(`Hi ${ mention.text }`);
+        const replyActivity = MessageFactory.text(`Hi ${mention.text}`);
         replyActivity.entities = [mention];
-        
+
         await context.sendActivity(replyActivity);
     }
 }
-
-module.exports.BotActivityHandler = BotActivityHandler;
-
